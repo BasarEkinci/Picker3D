ef66690e6d50d71d62d4bf848c57c802ad4eea98
BasarEkinci <basarekinci02@outlook.com>
2023-09-24 14:56:52 +0300

    Camera Manager Added

--- Picker3D/Assets/Scripts/Runtime/Managers/CameraManager.cs
+++ Picker3D/Assets/Scripts/Runtime/Managers/CameraManager.cs
@@ -1,9 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using Cinemachine;
+using Runtime.Signals;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace Runtime.Managers
 {
     public class CameraManager : MonoBehaviour
     {
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
         
+        private float3 _firstPosition;
+
+        private void Start()
+        {
+            Init();
+        }
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+        private void Init()
+        {
+            _firstPosition = transform.position;
+        }
+        private void UnsubscribeEvents()
+        {
+            CameraSignals.Instance.OnSetCameraTarget -= OnSetCameraTarget;
+            CoreGameSignals.Instance.OnReset -= OnReset;
+        }
+
+        private void SubscribeEvents()
+        {
+            CameraSignals.Instance.OnSetCameraTarget += OnSetCameraTarget;
+            CoreGameSignals.Instance.OnReset += OnReset;
+        }
+
+        private void OnReset()
+        {
+            transform.position = _firstPosition;
+        }
+
+        private void OnSetCameraTarget()
+        {
+            //var player = FindObjectOfType<PlayerManager>().transform;
+            //virtualCamera.Follow = player;
+            ////virtualCamera.LookAt = player;
+        }
     }
 }
--- Picker3D/Assets/Scripts/Runtime/Signals/CameraSignals.cs
+++ Picker3D/Assets/Scripts/Runtime/Signals/CameraSignals.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Runtime.Signals
 {
     public class CameraSignals : MonoBehaviour
     {
+        #region Singleton
+
+        public static CameraSignals Instance;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+            Instance = this;
+        }
+
+        #endregion
         
+        public UnityAction OnSetCameraTarget = delegate {  }; 
     }
 }
